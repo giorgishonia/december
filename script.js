8738708859a03f7d23a4ccb31684a260eb435aaa
shono <harden13mf@gmail.com>
2022-10-23 00:50:54 +0400

    magaria java (yavac)

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ console.log(markHigherBMI)
 // CODING CHALLENGE #2️⃣.
 
 // HEIGHT AND MASS VARIABLES
-let markHeight = 1.69;
+/* let markHeight = 1.69;
 let markMass = 78;
 let johnHeight = 1.95;
 let johnMass = 92;
@@ -42,5 +42,45 @@ if(markBMI > johnBMI){
 }else{
     console.log(`John's BMI (${johnBMI}) is higher than Mark's!`)
 }
+*//*
 
+//CODING CHALLENGE #3️⃣.
+
+let minimumScore = 100;
+
+let dolphinScoreOne = 96;
+let dolphinScoreTwo = 108;
+let dolphinScoreThree = 89;
+
+let averageDolphinScore = Math.trunc((dolphinScoreOne + dolphinScoreTwo + dolphinScoreThree ) / 3)
+console.log(averageDolphinScore);
+
+let koalaScoreOne = 88;
+let koalaScoreTwo = 91;
+let koalaScoreThree = 115;
+
+let averageKoalaScore = Math.trunc((koalaScoreOne + koalaScoreTwo + koalaScoreThree) / 3)
+console.log(averageKoalaScore)
+
+if(averageDolphinScore >= minimumScore && averageDolphinScore > averageKoalaScore){
+    console.log("delfinebma moiges")
+}else if (averageDolphinScore >= minimumScore && averageDolphinScore < averageKoalaScore) {
+        console.log("koalebma moiges")
+} else if (averageDolphinScore < minimumScore && averageDolphinScore == averageKoalaScore) {
+    console.log("qula ar eyot magram fre")
+} else if (averageDolphinScore < minimumScore && averageDolphinScore > averageKoalaScore) {
+    console.log("qula ar eyot magram delfinma ajoba")
+}
+else if (averageDolphinScore < minimumScore && averageDolphinScore < averageKoalaScore) {
+    console.log("qula ar eyot magram koalam ajoba")
+}
+else if(averageDolphinScore >= minimumScore && averageDolphinScore == averageKoalaScore){
+    console.log("fre")
+} else if (averageDolphinScore < minimumScore) {
+    console.log("qula ar eyot")
+}
+else{
+    console.log("es tu amoagdo mashin xui znaet brat")
+}
+*/
 
